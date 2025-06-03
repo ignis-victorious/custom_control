4e058181cc448d8d8cb08a9cfc8b93efaec37e2d
Emmanuele Levi <ignisvictorious@gmail.com>
2025-06-03 14:48:02 +0100

    Adjust centre

--- src/main.py
+++ src/main.py
@@ -1,23 +1,44 @@
-
 #  ___________________
 #  Import LIBRARIES
 import flet as ft
-from flet import Page, app, Text, SafeArea, Container
+from flet import Page, app, Text, Column
 #  Import FILES
 #  ___________________
 
 
-def main(page: Page):
-    text = Text("Hello Mum", size=50)
+class MyButton(ft.ElevatedButton):
+    def __init__(self, text: str, on_click: ft.OptionalControlEventCallable) -> None:
+        super().__init__(
+            text=text,
+            on_click=on_click,
+            bgcolor=ft.Colors.ORANGE_300,
+            color=ft.Colors.GREEN_800,
+        )
 
-    
+
+def main(page: Page) -> None:
+    page.title = "https://flet.dev/docs/getting-started/custom-controls/"
+    # page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    mum_text = Text(value="Hello Mum", size=50)
+
+    def ok_clicked(e: ft.ControlEvent) -> None:
+        print("OK clicked")
+
+    def cancel_clicked(e: ft.ControlEvent) -> None:
+        print("Cancel clicked")
+
     page.add(
-        SafeArea(
-            Container(
-                text,
-                alignment=ft.alignment.center,
-            ),
+        Column(
+            controls=[
+                MyButton(text="OK", on_click=ok_clicked),
+                mum_text,
+                MyButton(text="Cancel", on_click=cancel_clicked),
+            ],
             expand=True,
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         )
     )
 
